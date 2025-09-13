2aa93ee55048627b236db7d03e1f88593f47255b
renupe <95033058+renupe@users.noreply.github.com>
2025-09-13 22:37:36 +0530

    Add files via upload
    week1day1 Assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		//Declaration and Initialization
+		float version =91.0f;
+		String val="google";
+		Boolean beta =false;
+		int year=2008;
+		char shortCutKey='c';
+		// printing the values
+		System.out.println(version);
+		System.out.println(val);
+		System.out.println(beta);
+		System.out.println(year);
+		System.out.println(shortCutKey);
+		
+
+	}
+
+}
